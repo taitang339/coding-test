af26079771cf9f6092907bc05b412851b91b5d11
taitang339 <taitang339@gmail.com>
2020-11-24 07:05:21 -0700

    code-test-2

--- src/index.jsx
+++ src/index.jsx
@@ -27,6 +27,7 @@ function HousingInfoView (props){
 
         fetchData().catch( (error) => {
             alert("ERROR");
+            console.log(error);
         });
     }, [])
 
@@ -34,40 +35,52 @@ function HousingInfoView (props){
 
     function infoTable(){
 
-        if(data.communities){
-            const communityList = data.communities.map((community)=> 
-            <div class="col">
-                <div class="card text-center" style={{width: "18em"}}>
-                    <figure>
-                        <img class="figure-img img-fluid" src={community.imgUrl} alt="Card image cap"></img>
-                    </figure>
-                    <div class="card-body">
-                        <h5 class="card-title">{community.name}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            
-            );
+        let communityList;
+        let homeList;
+        let totalPrice = 0;
+        let averagePrice = 0;
+        let communityHomes;
 
+        if(data.communities && data.homes){
+            communityList = data.communities.sort((a, b) => a.name.localeCompare(b.name));
+            homeList = data.homes;
+          
+            
+        
+            communityList = data.communities.map((community)=> {
+                communityHomes = homeList.filter((home)=> home.communityId === community.id);
+                if(communityHomes.length){
+                    totalPrice = communityHomes.reduce((acc, current)=>{
+                        return acc + current.price; 
+                    }, 0) / communityHomes.length;
+                }
+                return (
+                    <div class="col">
+                        <div class="card text-center" style={{width: "18em"}}>
+                            <figure>
+                                <img class="figure-img img-fluid" src={community.imgUrl.length? community.imgUrl : "https://piotrkowalski.pw/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png"} alt="Card image cap"></img>
+                            </figure>
+                            <div class="card-body">
+                                <h5 class="card-title">{community.name}</h5>
+                                <p class="card-text">Average Price : {totalPrice}</p>
+                            </div>
+                        </div>
+                    </div>
+                );
+            });
+            
             return communityList;
         }
 
         return null;
     }
-
-    // const communityList = data.communities.map((community)=> 
-    //         <li>{community.name}</li>
-    // );
    
     return(
         <div class="container">
             <div class="row">
-            {infoTable()}
+                {infoTable()}
             </div>
         </div>
-        //<div>{infoTable()}</div>
-        // // <h1>HELLO</h1> */}
     );
    
 }
